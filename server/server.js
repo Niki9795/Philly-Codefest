b8c9d8b5f092eca1104fd98b4881feda1216f187
Niki <nikip9795@gmail.com>
2024-04-20 14:57:19 -0400

    -

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/message", (req, res) => {
     res.json({ message: "Hello from nodeJS!" });
