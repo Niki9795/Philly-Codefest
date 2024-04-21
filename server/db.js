e44dfb08a9d7dda025c9982093da79fd0585c75d
Niki <nikip9795@gmail.com>
2024-04-21 11:38:17 -0400

    final commit

--- server/db.js
+++ server/db.js
@@ -11,12 +11,14 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     },
     location: {
-        latitude: Number,
-        longitude: Number
-      },
+        locationName: { type: String, default: 'Belém do Candeia' },
+        latitude: { type: Number, default: -3.5146957460989574 }, // replace with your latitude
+        longitude: { type: Number, default: -62.46641126710576 } // replace with your longitude
+    },
     markers: { type: Array, default: [] },
-    count: { type: Array, default: [] },
+    count: { type: Number, default: 0 },
     landType: { type: String, default: null },
+    cause: { type: String, default:null},
 })
 
 module.exports = mongoose.model("users", userSchema);
