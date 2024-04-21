99780e86cc13e5c4dde911f6ffadb6cd30436612
Ujjwal Malik <ujjwal.malik2@gmail.com>
2024-04-21 03:08:22 -0400

    added homepage

--- client/src/AppPages/HomePage.js
+++ client/src/AppPages/HomePage.js
@@ -1,33 +1,43 @@
 import React from "react";
-import '../css/homepage.css';  // Importing CSS for styling
+import '../css/homepage.css';
 
 const HomePage = () => {
-  return(
+  return (
     <div className="homepage">
-      <div className="jumbotron jumbotron-fluid bg-dark text-white text-center">
-        <div className="container">
-          <h1 className="display-4">Guardian of the Greens</h1>
-          <p className="lead">Protecting forests and farms with cutting-edge acoustic AI.</p>
+      <div className="jumbotron">
+        <div className="jumbotron-content">
+          <h1>Conserve AI</h1>
+          <p>Protecting forests and farms with cutting-edge acoustic AI.</p>
         </div>
       </div>
-      <div className="container bg-custom" style={{color: '#333'}}>  
-        <div className="row content-section">
-          <div className="col-12">
-            <h2>About Our Technology</h2>
-            <p>Discover how our AI detects threats and intrusions to preserve natural habitats.</p>
-            <img src="path_to_an_explanatory_image.jpg" alt="Acoustic AI" className="img-fluid"/>
+      <div className="light-section">
+        <div className="content-container">
+          <div className="row content-section">
+            <div className="col-12 col-md-6">
+              <h2>About Our Technology</h2>
+              <p>Discover how our AI detects threats and intrusions to preserve natural habitats.</p>
+            </div>
+            <div className="col-12 col-md-6">
+              <img src="/acoustic.png" alt="Acoustic AI" className="img-fluid" />
+            </div>
           </div>
         </div>
-        <div className="row content-section">
-          <div className="col-12">
-            <h2>Real-time Monitoring</h2>
-            <p>Continuous surveillance ensures that any disturbance is caught early.</p>
-            <img src="path_to_monitoring_image.jpg" alt="Monitoring" className="img-fluid"/>
+      </div>
+      <div className="dark-section">
+        <div className="content-container">
+          <div className="row content-section">
+            <div className="col-12 col-md-6">
+              <img src="/map.jpg" alt="Monitoring" className="img-fluid" />
+            </div>
+            <div className="col-12 col-md-6">
+              <h2>Real-time Monitoring</h2>
+              <p>Continuous surveillance ensures that any disturbance is caught early.</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
